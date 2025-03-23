087a57ceaede48064cbccc5d971c2d1ec1c88965
Eshan Chatterjee <eshan2001221@gmail.com>
2025-03-23 13:01:41 +0530

    impl text Input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::ffi::CStr;
-use std::fs::File;
-use std::io::Read;
-use std::os::fd::OwnedFd;
+use std::os::fd::{AsRawFd, OwnedFd};
 
+use anyhow::anyhow;
 use eframe::egui;
+use itertools::Itertools;
+use nix::errno::Errno;
+use nix::fcntl;
 use nix::pty;
 use nix::pty::ForkptyResult;
 use nix::unistd::Pid;
@@ -21,7 +23,15 @@ fn main() -> anyhow::Result<()> {
             }
             ForkptyResult::Child => {
                 let shell = CStr::from_bytes_until_nul(b"/bin/bash\0")?;
-                let args = [CStr::from_bytes_until_nul(b"--noprofile\0")?];
+
+                let args: &[&[u8]] = &[b"--noprofile\0"];
+                let args = args
+                    .iter()
+                    .map(|value| {
+                        CStr::from_bytes_with_nul(value).expect("Should Have Null terminator")
+                    })
+                    .collect_vec();
+
                 // need to unwrap here because execvp never returns
                 nix::unistd::execvp(shell, &args).unwrap();
                 return Ok(());
@@ -35,44 +45,75 @@ fn main() -> anyhow::Result<()> {
     let _ = eframe::run_native(
         "tty",
         native_options,
-        Box::new(|cc| Ok(Box::new(TTY::new(cc, fd)))),
+        Box::new(|cc| Ok(Box::new(Tty::new(cc, fd)))),
     );
 
     Ok(())
 }
 
-struct TTY {
-    fd: File,
+struct Tty {
+    fd: OwnedFd,
     buffer: Vec<u8>,
 }
 
-impl TTY {
-    fn new(cc: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
+impl Tty {
+    fn new(_cc: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
+        let flags = nix::fcntl::fcntl(fd.as_raw_fd(), fcntl::FcntlArg::F_GETFL)
+            .expect("Should Be a valid fd here");
+        let mut flags = fcntl::OFlag::from_bits(flags & fcntl::OFlag::O_ACCMODE.bits())
+            .ok_or(anyhow!("Is not a vaild fd flags"))
+            .expect("Should be a valid flag here");
+
+        flags.set(fcntl::OFlag::O_NONBLOCK, true);
+
+        nix::fcntl::fcntl(fd.as_raw_fd(), fcntl::FcntlArg::F_SETFL(flags))
+            .expect("Should not error");
+
+        // TODO: Set fonts later
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
         Self {
-            fd: fd.into(),
+            fd,
             buffer: Vec::new(),
         }
     }
 }
 
-impl eframe::App for TTY {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+impl eframe::App for Tty {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let mut buf = vec![0u8; 4096];
-        match self.fd.read(&mut buf) {
+
+        match nix::unistd::read(self.fd.as_raw_fd(), &mut buf) {
             Ok(sz) => {
                 self.buffer.extend_from_slice(&buf[..sz]);
             }
             Err(err) => {
-                println!("Error reading from fd: {:?}", err);
+                if err != Errno::EAGAIN {
+                    println!("Error reading from fd: {:?}", err);
+                }
             }
         };
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            unsafe  {
+            ui.input(|input_reader| {
+                for event in &input_reader.events {
+                    let egui::Event::Text(text) = event else {
+                        continue;
+                    };
+                    let bytes = text.as_bytes();
+                    let mut to_write: &[u8] = bytes;
+
+                    while !to_write.is_empty() {
+                        let written = nix::unistd::write(&self.fd, to_write)
+                            .expect("Should be able to write");
+
+                        to_write = &to_write[written..];
+                    }
+                }
+            });
+            unsafe {
                 ui.label(std::str::from_utf8_unchecked(&self.buffer));
             }
         });
