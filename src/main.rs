df1a2d2046985bd5df14aefba5023db361cc8bd5
Eshan Chatterjee <eshan2001221@gmail.com>
2025-03-26 15:57:33 +0530

    basic shell io + cursor

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 mod utils;
 
-use std::ffi::CStr;
-use std::os::fd::{AsRawFd, OwnedFd};
-
 use anyhow::anyhow;
-use eframe::egui::{self, RichText};
+use eframe::egui::{self, Color32, Rect, RichText};
 use itertools::Itertools;
 use nix::errno::Errno;
 use nix::fcntl;
 use nix::pty;
 use nix::pty::ForkptyResult;
 use nix::unistd::Pid;
+use std::ffi::CStr;
+use std::os::fd::{AsRawFd, OwnedFd};
 
-use utils::{replace_font, add_font};
-
+use utils::{add_font, character_to_screen_pos, get_char_size, replace_font};
 
 fn main() -> anyhow::Result<()> {
     let fd = unsafe {
@@ -29,7 +27,7 @@ fn main() -> anyhow::Result<()> {
             ForkptyResult::Child => {
                 let shell = CStr::from_bytes_until_nul(b"/bin/bash\0")?;
 
-                let args: &[&[u8]] = &[b"--noprofile\0"];
+                let args: &[&[u8]] = &[b"bash\0", b"--noprofile\0"];
                 let args = args
                     .iter()
                     .map(|value| {
@@ -58,6 +56,8 @@ fn main() -> anyhow::Result<()> {
 
 struct Tty {
     fd: OwnedFd,
+    cursor_pos: (usize, usize),
+    char_size: Option<(f32, f32)>,
     buffer: Vec<u8>,
 }
 
@@ -81,17 +81,30 @@ impl Tty {
         Self {
             fd,
             buffer: Vec::new(),
+            char_size: None,
+            cursor_pos: (0, 0),
         }
     }
 }
 
 impl eframe::App for Tty {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        if self.char_size.is_none() {
+            self.char_size = Some(get_char_size(ctx));
+            println!("Char Size: {:?}", self.char_size);
+        }
         let mut buf = vec![0u8; 4096];
 
         match nix::unistd::read(self.fd.as_raw_fd(), &mut buf) {
             Ok(sz) => {
-                self.buffer.extend_from_slice(&buf[..sz]);
+                let incomming = &buf[..sz];
+                for c in incomming {
+                    match c {
+                        b'\n' => self.cursor_pos = (0, self.cursor_pos.1 + 1),
+                        _ => self.cursor_pos = (self.cursor_pos.0 + 1, self.cursor_pos.1),
+                    }
+                }
+                self.buffer.extend_from_slice(incomming);
             }
             Err(err) => {
                 if err != Errno::EAGAIN {
@@ -103,9 +116,34 @@ impl eframe::App for Tty {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.input(|input_reader| {
                 for event in &input_reader.events {
-                    let egui::Event::Text(text) = event else {
-                        continue;
+                    let text = match event {
+                        egui::Event::Text(text) => text,
+                        egui::Event::Key {
+                            key: egui::Key::Enter,
+                            pressed: true,
+                            ..
+                        } => "\n",
+
+                        // egui::Event::Copy => todo!(),
+                        // egui::Event::Cut => todo!(),
+                        // egui::Event::Paste(_) => todo!(),
+                        // egui::Event::PointerMoved(pos2) => todo!(),
+                        // egui::Event::MouseMoved(vec2) => todo!(),
+                        // egui::Event::PointerButton { pos, button, pressed, modifiers } => todo!(),
+                        // egui::Event::PointerGone => todo!(),
+                        // egui::Event::Zoom(_) => todo!(),
+                        // egui::Event::Ime(ime_event) => todo!(),
+                        // egui::Event::Touch { device_id, id, phase, pos, force } => todo!(),
+                        // egui::Event::MouseWheel { unit, delta, modifiers } => todo!(),
+                        // egui::Event::WindowFocused(_) => todo!(),
+                        // egui::Event::AccessKitActionRequest(action_request) => todo!(),
+                        // egui::Event::Screenshot { viewport_id, user_data, image } => todo!(),
+                        _ => "",
                     };
+
+                    // let egui::Event::Text(text) = event else {
+                    //     continue;
+                    // };
                     let bytes = text.as_bytes();
                     let mut to_write: &[u8] = bytes;
 
@@ -118,7 +156,21 @@ impl eframe::App for Tty {
                 }
             });
 
-            ui.label(RichText::new(String::from_utf8_lossy(&self.buffer)));
+            let resp = ui.label(RichText::new(String::from_utf8_lossy(&self.buffer)));
+            let bottom = resp.rect.bottom();
+            let left = resp.rect.left();
+            let painter = ui.painter();
+            let char_size = self.char_size.as_ref().unwrap();
+            let cursor_offsets = character_to_screen_pos(&self.cursor_pos, char_size, &self.buffer);
+
+            painter.rect_filled(
+                Rect::from_min_size(
+                    egui::pos2(left + cursor_offsets.0, bottom + cursor_offsets.1),
+                    egui::vec2(char_size.0, char_size.1),
+                ),
+                0.0,
+                Color32::GRAY,
+            )
         });
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,10 @@
-use std::sync::Arc;
+use std::{f32, sync::Arc};
 
 use eframe::{
-    egui::{Context, FontData, FontDefinitions, FontFamily},
+    egui::{self, Color32, Context, FontData, FontDefinitions, FontFamily},
     epaint::text::{FontInsert, FontPriority, InsertFontFamily},
 };
+use itertools::Itertools;
 
 const JETBRAINS_MONO_NF: &'static str = "JetbrainsMonoNG";
 
@@ -48,4 +49,27 @@ pub fn replace_font(ctx: &Context) {
         .push(JETBRAINS_MONO_NF.to_string());
 
     ctx.set_fonts(fonts);
+}
+
+pub fn get_char_size(ctx: &Context) -> (f32, f32) {
+    let font_id = ctx.style().text_styles[&egui::TextStyle::Monospace].clone();
+    let (width, height) = ctx.fonts(|fonts| {
+        let layout = fonts.layout("@".to_string(), font_id, Color32::default(), f32::INFINITY);
+        (layout.rect.width(), layout.rect.height())
+    });
+
+    (width, height)
+}
+
+pub fn character_to_screen_pos(
+    char_pos: &(usize, usize),
+    char_size: &(f32, f32),
+    content: &[u8],
+) -> (f32, f32) {
+    let content_by_lines = content.split(|char| *char == b'\n').collect_vec();
+    let num_lines = content_by_lines.len();
+    let x_offset = char_pos.0 as f32 * char_size.0;
+    let y_offset = (char_pos.1 as i64 - num_lines as i64) as f32 * char_size.1;
+
+    (x_offset, y_offset)
 }
